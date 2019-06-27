215e6a379091ef6b3bb9a51ba9353fb79fff1c29
Hao Teng <haoteng10@gmail.com>
2019-06-27 18:12:10 -0400

    Fixed the left border

--- index.js
+++ index.js
@@ -2,17 +2,23 @@ function animation(index) {
     var selectedDisplayGrid = document.getElementsByClassName("display-grid")[index];
     
     var selectedSlider = selectedDisplayGrid.getElementsByClassName("slider")[0];
-    console.log(selectedSlider);
 
     selectedSlider.classList.add('slider-active');
-    console.log(selectedSlider.classList);
     setTimeout(function(){
         selectedSlider.classList.remove('slider-active');
     }, 4000); 
 
+    selectedDisplayGrid.classList.add('display-grid-active');
+    setTimeout(function(){
+        selectedDisplayGrid.classList.remove('display-grid-active');
+        console.log("executed!");
+    }, 4000); 
+    
     var selectedContentContainer = selectedDisplayGrid.getElementsByClassName("content-container")[0];
     selectedContentContainer.classList.add('content-container-active');
     setTimeout(function(){
         selectedContentContainer.classList.remove('content-container-active');
     }, 4000); 
+
+    
 }
