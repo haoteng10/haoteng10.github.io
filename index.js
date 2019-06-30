699c8e35b1999d6e4f788a8c160f8de66ba2a55c
Hao Teng <haoteng10@gmail.com>
2019-06-29 22:34:35 -0400

    More fixes

--- index.js
+++ index.js
@@ -2,23 +2,18 @@ function animation(index) {
     var selectedDisplayGrid = document.getElementsByClassName("display-grid")[index];
     
     var selectedSlider = selectedDisplayGrid.getElementsByClassName("slider")[0];
+    var selectedContentContainer = selectedDisplayGrid.getElementsByClassName("content-container")[0];
 
     selectedSlider.classList.add('slider-active');
+    selectedDisplayGrid.classList.add('display-grid-active');
+    selectedContentContainer.classList.add('content-container-active');
+
     setTimeout(function(){
         selectedSlider.classList.remove('slider-active');
-    }, 4000); 
-
-    selectedDisplayGrid.classList.add('display-grid-active');
-    setTimeout(function(){
-        selectedDisplayGrid.classList.remove('display-grid-active');
-        console.log("executed!");
-    }, 6000); 
-    
-    var selectedContentContainer = selectedDisplayGrid.getElementsByClassName("content-container")[0];
-    selectedContentContainer.classList.add('content-container-active');
-    setTimeout(function(){
         selectedContentContainer.classList.remove('content-container-active');
+        setTimeout(function(){
+            selectedDisplayGrid.classList.remove('display-grid-active');
+        }, 2000); 
     }, 4000); 
 
-    
 }
